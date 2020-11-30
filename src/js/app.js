7e29ab12f65b6d7e3e7e31b59d0585ea94c48bf3
FakeFunnyFace <pashaa2005@gmail.com>
2020-11-30 17:54:08 +0500

    upbutton

--- src/js/app.js
+++ src/js/app.js
@@ -22,6 +22,27 @@ $(window).scroll(function(){
 
 
 
+var smoothJumpUp = function() {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+      window.scrollBy(0,-50);
+      setTimeout(smoothJumpUp, 1);
+  }
+}
+
+window.onscroll = function() {
+var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+if (scrolled > 100) {
+  document.getElementById('upbutton').style.display = 'block';
+} else {
+  document.getElementById('upbutton').style.display = 'none';
+}
+}
+
+
+
+
+
+
 
 
 
